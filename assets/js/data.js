404dab4284bc405c1a8359bf95d2704697392c79
711stoner <juejiangfm@gmail.com>
2026-04-28 19:22:15 +0800

    update: 《机器》MV标题改为【音乐】《机器》-MV

--- assets/js/data.js
+++ assets/js/data.js
@@ -244,7 +244,7 @@ window.WORKS = [
   },
   {
     "slug": "mv-machine",
-    "title_zh": "《机器》MV",
+    "title_zh": "【音乐】《机器》-MV",
     "title_en": "",
     "role_zh": "",
     "role_en": "Machine MV",

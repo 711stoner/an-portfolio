d546ec5309129f17568fe92dd4ac2e28de018ed4
711stoner <juejiangfm@gmail.com>
2026-04-28 19:05:52 +0800

    update: 调整分类顺序

--- assets/js/data.js
+++ assets/js/data.js
@@ -790,9 +790,9 @@ window.WORKS = [
 window.WORK_CATEGORIES = [
   { key: "All", label: "全部 / All" },
   { key: "Documentary", label: "纪录/访谈 / Documentary" },
-  { key: "ShortVideo", label: "短视频 / Short Video" },
   { key: "Commercial", label: "商业广告 / Commercial" },
   { key: "Short", label: "短片 / Videos" },
+  { key: "ShortVideo", label: "短视频 / Short Video" },
 ];
 
 window.WORK_YEARS = [
